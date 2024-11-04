9f1f168f7c1ec6228387f2444cc2e636cee6f410
Hanna Sampaio <130690107+hannasampaio@users.noreply.github.com>
2024-11-04 11:33:36 +0000

    construindo o hero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,42 @@
-console.log("Olá mundo!");
+const dataDoEvento = new Date("jun 1, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.querySelector('.hero__contador').innerHTML = `
+        <div class="hero__contador__item">
+            ${diasAteOEvento}
+            <span class="hero__contador__item__label">Days</span>
+        </div>
+        <div class="hero__contador__item">
+            ${horasAteOEvento}
+            <span class="hero__contador__item__label">Hours</span>
+        </div>
+        <div class="hero__contador__item">
+            ${minutosAteOEvento}
+            <span class="hero__contador__item__label">Minutes</span>
+        </div>
+        <div class="hero__contador__item">
+            ${segundosAteOEvento}
+            <span class="hero__contador__item__label">Seconds</span>
+        </div>
+    `;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.querySelector('.hero__contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
